d493a9da6518baa61298f5f4188067b749b74b42
jkalandarov <jkalandarov@gmail.com>
2022-03-09 18:48:53 +0500

    Assertion checks moved to their own it()

--- task_14/test/home.js
+++ task_14/test/home.js
@@ -10,74 +10,91 @@ let expirationTime = '10 Minutes'
 
 describe('I Can Win', function(){
     const Home = new HomePage()
-    let pastedText = 'Hello from WebDriver'
-    let pageTitle = 'helloweb'
+    let pastedText = 'Hello from WebDriver';
+    let pageTitle = 'helloweb';
 
     it('Open https://pastebin.com', function(){
-        HomePage.goToUrl(baseUrl)
-    })
+        HomePage.goToUrl(baseUrl);
+    });
 
     it('Paste "Hello from WebDriver"', function(){
-        HomePage.enterPaste('postform-text', pastedText)
-    })
+        HomePage.enterPaste('postform-text', pastedText);
+    });
 
     it('set highlightning', function(){
-        HomePage.setSyntax('select2-postform-format-container', 'select2-search__field', syntax)
-    })
+        HomePage.setSyntax('select2-postform-format-container', 'select2-search__field', syntax);
+    });
 
     it('Paste Expiration: "10 Minutes"', function(){
-        HomePage.setExpiration('select2-postform-expiration-container', expirationTime)
-    })
+        HomePage.setExpiration('select2-postform-expiration-container', expirationTime);
+    });
 
     it('Paste name: helloweb', function(){
-        HomePage.pasteName('postform-name', pageTitle)
-    })
+        HomePage.pasteName('postform-name', pageTitle);
+    });
 
     it('Click "Create New Paste"', function(){
-        HomePage.clickButton()
-    })
+        HomePage.clickButton();
+    });
 
-    it('Assertion checks', function() {
-        expect(BasePage.driver.getTitle()).to.equal(pageTitle)
-        expect(ResultPage.selectBashIconByXpath.getText()).to.equal(syntax)
-        expect(ResultPage.selectTimeElementByClass.getText()).to.equal(expirationTime)
-        expect(ResultPage.selectPastedTextByClass.getText()).to.equal(pastedText)
-    })
+    it("check page title", function(){
+        expect(BasePage.driver.getTitle()).to.equal(pageTitle);
+    });
 
+    it("should have a proper syntax chosen", function() {
+        expect(ResultPage.selectBashIconByXpath.getText()).to.equal(syntax);
+    });
+
+    it("should have expiration time set", function(){
+        expect(ResultPage.selectTimeElementByClass.getText()).to.equal(expirationTime);
+    });
+
+    it('should contain pasted text value', function(){
+        expect(ResultPage.selectPastedTextByClass.getText()).to.equal(pastedText);
+    });
 })
 
 describe('Bring it on', function(){
     this.timeout(5000)
     let pastedText = 'git config --global user.name "New Sheriff in Town" \n git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code") \n git push origin master --force'
-    let pageTitle = 'how to gain dominance among developers'
+    let pageTitle = 'how to gain dominance among developers';
     it('Open https://pastebin.com', function(){
         HomePage.goToUrl(baseUrl)
-    })
+    });
 
     it('Create a New Paste with the following details', function(){
         HomePage.enterPaste('postform-text', pastedText)
-    })
+    });
 
     it('Set Syntax Highlighting: "Bash"', function(){
         HomePage.setSyntax('select2-postform-format-container', syntax)
-    })
+    });
 
     it('Paste Expiration: "10 Minutes"', function(){
         HomePage.setExpiration('select2-postform-expiration-container', expirationTime)
-    })
+    });
 
     it('Paste name: how to gain dominance among developers', function(){
         HomePage.pasteName('postform-name', pageTitle)
-    })
+    });
 
     it('Click "Create New Paste"', function(){
         HomePage.clickButton()
-    })
+    });
 
-    it('Assertion checks', function(){
+    it("check page title", function(){
         expect(BasePage.driver.getTitle()).to.equal(pageTitle)
+    });
+
+    it("should have a proper syntax chosen", function() {
         expect(ResultPage.selectBashIconByXpath.getText()).to.equal(syntax)
+    });
+
+    it("should have expiration time set", function(){
         expect(ResultPage.selectTimeElementByClass.getText()).to.equal(expirationTime)
+    });
+
+    it('should contain pasted text value', function(){
         expect(ResultPage.selectPastedTextByClass.getText()).to.equal(pastedText)
-    })
+    });
 })
